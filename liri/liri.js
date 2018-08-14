8f89cbb3b0a9d0dc38e29669ce934f4e3f5797a1
Tals <natalia.mahabir@gmail.com>
2018-08-13 22:20:44 -0400

    npm packages installed and liri app updated with variables

--- liri/liri.js
+++ liri/liri.js
@@ -1,4 +1,8 @@
-require("dotenv").config();
+var request = require("dotenv").config();
 var key = require("./key.js");
 var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
+
+var input1 = process.argv[2];
+var input2 = process.argv[3];
+// console.log(process.argv);
