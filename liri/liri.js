0d68e832d81d1a56dab23a7045474bba50e316b3
Tals <natalia.mahabir@gmail.com>
2018-08-14 00:46:20 -0400

    spotify loop updated, ace of base song to be updated

--- liri/liri.js
+++ liri/liri.js
@@ -13,10 +13,54 @@ var queryURL =
   "http://www.omdbapi.com/?t=" + input2 + "&y=&plot=short&apikey=trilogy";
 
 switch (input1) {
-  case "spotify-this-song":
+  case "spotify-this-song" && input2 !== undefined:
     spotify.search({ type: "track", query: input2 }, function(err, data) {
       if (err) {
         return console.log("Spotify Error: " + err);
-      } else console.log(data);
+      } else {
+        // console.log(data.tracks.items[1]);
+        // Returns the data for the first result
+        var album = data.tracks.items[1].album.name;
+        var artist = data.tracks.items[1].artists[0].name;
+        var title = data.tracks.items[1].name;
+        var preview = data.tracks.items[1].preview_url;
+        return console.log(
+          "Song Title: " +
+            title +
+            " Artist(s): " +
+            artist +
+            " Album: " +
+            album +
+            " Preview Link: " +
+            preview
+        );
+      }
+    });
+
+  case "spotify-this-song":
+    spotify.search({ type: "track", query: "Ace of Base" }, function(
+      err,
+      data
+    ) {
+      if (err) {
+        return console.log("Spotify Error: " + err);
+      } else {
+        // console.log(data.tracks.items[1]);
+        // Returns the data for the first result
+        var album = data.tracks.items[1].album.name;
+        var artist = data.tracks.items[1].artists[0].name;
+        var title = data.tracks.items[1].name;
+        var preview = data.tracks.items[1].preview_url;
+        return console.log(
+          "Song Title: " +
+            title +
+            " Artist(s): " +
+            artist +
+            " Album: " +
+            album +
+            " Preview Link: " +
+            preview
+        );
+      }
     });
 }
