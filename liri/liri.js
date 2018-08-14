270f7873c095133774ebebecc8931aa99c10929a
Tals <natalia.mahabir@gmail.com>
2018-08-13 23:24:28 -0400

    switch for spotify started

--- liri/liri.js
+++ liri/liri.js
@@ -1,8 +1,22 @@
-var request = require("dotenv").config();
+var request = require("request");
+var dotenv = require("dotenv").config();
+var Spotify = require("node-spotify-api");
+var Twitter = require("twitter");
 var key = require("./key.js");
-var spotify = new Spotify(keys.spotify);
-var client = new Twitter(keys.twitter);
 
 var input1 = process.argv[2];
 var input2 = process.argv[3];
-// console.log(process.argv);
+
+var spotify = new Spotify(key.spotify);
+var client = new Twitter(key.twitter);
+var queryURL =
+  "http://www.omdbapi.com/?t=" + input2 + "&y=&plot=short&apikey=trilogy";
+
+switch (input1) {
+  case "spotify-this-song":
+    spotify.search({ type: "track", query: input2 }, function(err, data) {
+      if (err) {
+        return console.log("Spotify Error: " + err);
+      } else console.log(data);
+    });
+}
